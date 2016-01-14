73b50a526b660701e04e33679f6c4736d6a336bb
Jack Rimell <jackrimell123@gmail.com>
2016-01-14 22:55:17 +0000

    copied over Rownd docs website for the starter kit

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,152 +7,40 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> */',
 
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      jsLibs: {
-        src:
-          [
-            'node_modules/rownd/dist/rownd.js'
-            // Add js libs here
-          ],
-        dest: 'dist/assets/js/project-libs.js'
-      },
-      app: {
-        src: [
-          'app/routes.js',
-          'app/helpers/**/*.js',
-          'app/controllers/**/*.js'
-        ],
-        dest: 'dist/assets/js/project-app.js'
-      }
-    },
-
-    ractive: {
-      template: {
-        src: 'app/views/**/*.handlebars',
-        dest: 'dist/assets/js/project-templates.js'
-      }
-    },
-
-    copy: {
-      index: {
-        files: [{
-          expand: true,
-          cwd: 'app/',
-          src: ['index.html'],
-          dest: 'dist/'
-        }]
-      },
-      ractiveMap: {
-        files: [{
-          expand: true,
-          cwd: 'libs/ractive/',
-          src: ['ractive.js.map'],
-          dest: 'dist/assets/js/'
-        }]
-      },
-      styles: {
-        files: [{
-          expand: true,
-          cwd: 'app/styles/',
-          src: ['**.css'],
-          dest: 'dist/assets/css/'
-        }]
-      }
-    },
-
-    jshint: {
-      options : {
-        jshintrc : '.jshintrc'
-      },
-      beforeconcat: [
-        'app/routes.js',
-        'app/helpers/**/*.js',
-        'app/controllers/**/*.js'
-      ]
-    },
-
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      libs: {
+    less: {
+      dev: {
         files: {
-          'dist/assets/js/project-libs.js' : 'dist/assets/js/project-libs.js'
+          "style.css" : "styles/style.less"
         }
-      },
-      app: {
+      }
+    },
+
+    cssmin: {
+      target: {
         files: {
-          'dist/assets/js/project-app.js' : 'dist/assets/js/project-app.js'
+          'style.min.css': ['style.css']
         }
       }
     },
 
     watch: {
-      index: {
-        files: ['app/{,*/}*.html'],
-        tasks: ['copy:index', 'targethtml:dev']
-      },
-      app: {
-        files: ['app/**/*.js'],
-        tasks: ['concat:app', 'jshint']
-      },
-      templates: {
-        files: ['app/views/**/*.handlebars'],
-        tasks: ['ractive']
+      less: {
+        files: ['styles/*.less'],
+        tasks: ['less', 'cssmin']
       }
     }
 
   });
-
-    // The ractive task - Should probably be moved to it's own repo
-    // or replaced with a working one
-    var desc = 'Compile ractive.js templates',
-      path = require('path'),
-      Ractive = require('ractive');
-
-    grunt.registerMultiTask('ractive', desc, make);
-    function make(){
-        this.files.forEach(function(file){
-            var templates = file.src.map(parse);
-            grunt.file.write(file.dest,
-                'Rownd.templates = {\n' + templates.join(',\n') + '\n}');
-        });
-    }
-
-    function parse(template){
-        var name = path.basename(template, '.handlebars'),
-            html = grunt.file.read(template),
-            parsed = Ractive.parse(html);
-
-        return  '\t' + name + ': ' + JSON.stringify(parsed);
-    }
-    // End of ractive task
 
   // Load all of Grunt's dependencies
   require('matchdep')
     .filterDev('grunt-*')
     .forEach(grunt.loadNpmTasks);
 
-  // With watch
-  grunt.registerTask('watch', [
-    'concat',
-    'jshint',
-    'ractive',
-    'copy',
-    'watch']
-  );
-
-  // No watch
   grunt.registerTask('default', [
-    'concat',
-    'jshint',
-    'uglify',
-    'ractive',
-    'copy']
+    'less',
+    'cssmin',
+    'watch']
   );
 
 };
--- app/helpers/helpers.js
+++
@@ -1,7 +0,0 @@
-
-/* This is where re-usable helpers go that you can
- * use in multiple controllers.
-*/
-App.Helpers.helperExample = function(){
-  console.log('This is an example of a helper.');
-};
